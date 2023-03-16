b674c06e17a039559d84312c05010eb102a33004
kun1w <kun1w@WIN-KAAG924DIIQ>
2023-03-16 16:48:17 +0900

    example

---
+++ assignment3/src/assignment3/assignment3.java
@@ -0,0 +1,15 @@
+package assignment3;
+
+public class assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Add Football Player");
+		System.out.println("Delete Football Player");
+		System.out.println("Edit Football Player");
+		System.out.println("View Football Player");
+		System.out.println("Exit Football Player");
+
+	}
+
+}
