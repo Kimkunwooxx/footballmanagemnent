b41aa66bda5dff967cd9468b5d46fc1dd91d1ef4
Kimkunwooxx <126757838+Kimkunwooxx@users.noreply.github.com>
2023-03-23 14:44:19 +0900

    Update assignment3.java

--- assignment3/src/assignment3/assignment3.java
+++ assignment3/src/assignment3/assignment3.java
@@ -4,11 +4,11 @@ public class assignment3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Add Football Player");
-		System.out.println("Delete Football Player");
-		System.out.println("Edit Football Player");
-		System.out.println("View Football Player");
-		System.out.println("Exit Football Player");
+		System.out.println("1.Add Football Player");
+		System.out.println("2.Delete Football Player");
+		System.out.println("3.Edit Football Player");
+		System.out.println("4.View Football Player");
+		System.out.println("5.Exit Football Player");
 
 	}
 
